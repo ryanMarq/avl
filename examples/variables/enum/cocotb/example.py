9acce7b859d328daca9be61a02022dc9f6c0c719
Ryan Marquess <ryanjmar@outlook.com>
2025-07-10 12:17:56 +0100

    Add example for enum width

--- examples/variables/enum/cocotb/example.py
+++ examples/variables/enum/cocotb/example.py
@@ -12,7 +12,8 @@ class example_env(avl.Env):
     def __init__(self, name, parent):
         super().__init__(name, parent)
 
-        self.e = avl.Enum("e", "A", {"A": 0, "B": 1, "C": 2})
+        self.e1 = avl.Enum("e1", "A", {"A": 0, "B": 1, "C": 2}, width=4)
+        self.e2 = avl.Enum("e2", value="B", values={"A" : 127, "B" : -128})
 
 
 @cocotb.test
@@ -20,14 +21,21 @@ async def test(dut):
     e = example_env("env", None)
 
     # Direct allocation - must use .value to avoid trampling the class
-    e.e.value = e.e.B
-    assert e.e == e.e.B
+    e.e1.value = e.e1.B
+    assert e.e1 == e.e1.B
 
     # Increment
-    e.e += 1
-    assert e.e == e.e.C
+    e.e1 += 1
+    assert e.e1== e.e1.C
 
     # Random
     for _ in range(10):
-        e.e.randomize()
-        assert e.e in e.e.values.values()
+        e.e1.randomize()
+        assert e.e1 in e.e1.values.values()
+
+    # Enum Width
+    assert e.e1.width == 4
+    assert e.e2.width == 8
+
+    
+
