39cd2645556e5c0ad18b3ebc7f33786ebcb2431a
Ryan Marquess <ryanjmar@outlook.com>
2025-08-29 20:35:54 +0100

    refactor: simplify factory example for docs

--- examples/factory/variables/cocotb/example.py
+++ examples/factory/variables/cocotb/example.py
@@ -12,26 +12,19 @@ class example_env(avl.Env):
     def __init__(self, name, parent):
         super().__init__(name, parent)
 
-        self.a = avl.Factory.get_variable(f"{self.get_full_name()}.a")
+        # Get variable env.a in factory (set in the test method)
+        self.a = avl.Factory.get_variable(f"{self.get_full_name()}.a") 
 
         if self.a != 100:
             self.error(f"Expected a to be 100, got {self.a}")
 
-
-        self.b = avl.Factory.get_variable(f"{self.get_full_name()}.b", 200)
+        # Get variable env.b from the factory. Because it was never set, use the default
+        # value 200 instead.
+        self.b = avl.Factory.get_variable(f"{self.get_full_name()}.b", default=200)
 
         if self.b != 200:
             self.error(f"Expected b to be 200, got {self.b}")
-        
 
-        try:
-            avl.Factory.get_variable(f"{self.get_full_name()}.c")
-            missing_variable_errors = False
-        except KeyError:
-            missing_variable_errors = True
-
-        if missing_variable_errors is False:
-            self.error(f"Excepted missing varaible to error. It did not.")
 
 
 @cocotb.test
